d75900f1ee06a54db7ac402301cc7dc607093b07
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2025-12-19 12:51:02 +0700

    membuat code DestinasiHome

---
+++ app/src/main/java/com/example/pertemuan12/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.pertemuan12.uicontroller.route
+
+import com.example.ucp2_pam.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
