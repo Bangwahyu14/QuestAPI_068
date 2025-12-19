128e0c817a8d02649e621b3c888bc1610071f90d
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2025-12-19 12:55:37 +0700

    membuat code fun HostNavigasi

--- app/src/main/java/com/example/pertemuan12/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/pertemuan12/uicontroller/PetaNavigasi.kt
@@ -15,3 +15,21 @@ fun DataSiswaApp(navController: NavHostController = rememberNavController(), mod
     HostNavigasi(navController = navController)
 }
 
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(navController = navController, startDestination = DestinasiHome.route, modifier = Modifier ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                navigateToItemUpdate = {
+//                    navController.navigate("${DestinasiDetail.route}/${it}")
+                })
+        }
+        composable(DestinasiEntry.route) {
+            EntrySiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route) })
+        }
+    }
+}
