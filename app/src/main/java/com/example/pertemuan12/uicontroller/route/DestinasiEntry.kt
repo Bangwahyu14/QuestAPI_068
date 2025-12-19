71c3c66bf6eb72f7fe4d9043662dca532fc3ceb8
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2025-12-19 12:50:01 +0700

    membuat code Destinasientry

---
+++ app/src/main/java/com/example/pertemuan12/uicontroller/route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.example.pertemuan12.uicontroller.route
+
+import com.example.localrestapi.R
+import com.example.ucp2_pam.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
