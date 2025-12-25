8af07978801bf6169d0dc85ee3560272f68f85b2
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2025-12-25 16:41:46 +0700

    menambahkan code destinasi edit

---
+++ app/src/main/java/com/example/pertemuan12/uicontroller/route/DestinasiEdit.kt
@@ -0,0 +1,13 @@
+package com.example.pertemuan12.uicontroller.route
+
+import com.example.localrestapi.R
+
+object DestinasiEdit : DestinasiNavigasi {
+
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+
+}
